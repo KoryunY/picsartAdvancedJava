105bd1f9cf1c7f6fae0c96308c0896c326089f37
KoryunY <81442008+KoryunY@users.noreply.github.com>
2021-06-27 20:02:28 +0400

    Add files via upload

--- FolderSizeCalculator/FolderSizePrinter.java
+++ FolderSizeCalculator/FolderSizePrinter.java
@@ -9,14 +9,14 @@ public class FolderSizePrinter extends Thread{
     @Override
     public void run() {
         while (fsc.isAlive()){
-        System.out.println( "("+(int)FolderSizeCalculator.getSize() + "bytes"+")");
+        System.out.println( "("+(int)(FolderSizeCalculator.getSize()/1024) + "Kb"+")");
         try {
             sleep(100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
-        System.out.println("Your Folder Size is:"+"("+(int)FolderSizeCalculator.getSize() + "bytes"+")");
+        System.out.println("Your Folder Size is:"+"("+(int)(FolderSizeCalculator.getSize()/1024) + "Kb"+")");
 
     }
 }
